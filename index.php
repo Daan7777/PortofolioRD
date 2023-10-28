45f5e273d58d4aa30143cdd67671393bfed2a8c6
Daan7777 <Detvanmontfoort@gmail.com>
2023-10-28 21:23:24 +0200

    tailwind/changes

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+  <link rel="icon" type="image/png" sizes="16x16" href="/src/img/favicon.ico">
+</head>
+<?php
+include
+?>
+  
+  
+<body>
+</body>
+</html>
+<?php    ?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/*.{html,js,php}"],
+  content: ["./*.{html,js,php}"],
   theme: {
     extend: {},
   },

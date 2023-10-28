6f1c4457bcdc7087a12e632791805ad9aa865d60
Daan7777 <Detvanmontfoort@gmail.com>
2023-10-28 21:23:09 +0200

    navbar/favicon

---
+++ includes/navbar.php
@@ -0,0 +1,44 @@
+<nav class="m-6">
+  <div class="flex flex-wrap justify-between md:flex-nowrap md:gap-10">
+    <div class="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="" rel="" href="/">
+        Home
+      </a>
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="" rel="" href="/about">
+        About
+      </a>
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="" rel="" href="/contact">
+        Contact
+      </a>
+    </div>
+    <div class="flex w-full items-center justify-between md:w-auto"><a class="w-28 dark:hidden" href="/">
+      <img src="https://stablo.web3templates.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fcijrdavx%2Fproduction%2Fe8fa4f57a95067e838d7aa5a4f80042137d9f5b6-132x52.svg%3Fw%3D132%26auto%3Dformat&w=640&q=75" alt="">
+    </a>
+    <a class="hidden w-28 dark:block" href="/">
+      <img src="https://stablo.web3templates.com/_next/image?url=https%3A%2F%2Fcdn.sanity.io%2Fimages%2Fcijrdavx%2Fproduction%2Fe8fa4f57a95067e838d7aa5a4f80042137d9f5b6-132x52.svg%3Fw%3D132%26auto%3Dformat&w=640&q=75" alt="">
+    </a>
+    <button aria-label="Toggle Menu" class="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden " id="headlessui-disclosure-button-:r0:" type="button" aria-expanded="false" data-headlessui-state="">
+      <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+      </svg>
+    </button>
+    </div>
+    <div class="order-2 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row">
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="" rel="" href="/archive">
+        <span> 
+          Archive
+        </span>
+      </a>
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="_blank" rel="noopener" href="https://stablo-pro.web3templates.com/">
+        <span> 
+          Pro Version
+        </span>
+      </a>
+      <a class="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-600" target="_blank" rel="noopener" href="https://web3templates.com/templates/stablo-minimal-blog-website-template">
+        <span> 
+          Download
+        </span>
+      </a>
+    </div>
+  </div>
+</nav>
